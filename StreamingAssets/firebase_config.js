1abc43fbeecf07af8ddb885e45454d6d00c885ae
askrnout <rizky@arsanasia.com>
2022-01-06 17:15:55 +0700

    he

--- StreamingAssets/firebase_config.js
+++ StreamingAssets/firebase_config.js
@@ -1,12 +1,12 @@
 // Your web app's Firebase configuration,
 var firebaseConfig = {
-	apiKey: "AIzaSyAnr7m38RN28A6mg-IMG6rYURL6OLMncp8",
-	authDomain: "dcity-paras.firebaseapp.com",
-	databaseURL: "https://dcity-paras-default-rtdb.asia-southeast1.firebasedatabase.app",
-	projectId: "dcity-paras",
-	storageBucket: "dcity-paras.appspot.com",
-	messagingSenderId: "24940027159",
-	appId: "1:24940027159:web:359a14cbca04b6ea7feb52"
+	apiKey: "AIzaSyAihLKpowTdppnBsTA_mE0MrtnV126q9rg",
+	authDomain: "dcity-uniqueone.firebaseapp.com",
+	databaseURL: "https://dcity-uniqueone-default-rtdb.asia-southeast1.firebasedatabase.app",
+	projectId: "dcity-uniqueone",
+	storageBucket: "dcity-uniqueone.appspot.com",
+	messagingSenderId: "199308763432",
+	appId: "1:199308763432:web:da84ae626f978b638cb01d"
 };
 // Initialize Firebase,
 firebase.initializeApp(firebaseConfig);
---
+++ StreamingAssets/firebase_config_METAMASK.js
@@ -0,0 +1,12 @@
+// Your web app's Firebase configuration,
+var firebaseConfig = {
+	apiKey: "AIzaSyAihLKpowTdppnBsTA_mE0MrtnV126q9rg",
+	authDomain: "dcity-uniqueone.firebaseapp.com",
+	databaseURL: "https://dcity-uniqueone-default-rtdb.asia-southeast1.firebasedatabase.app",
+	projectId: "dcity-uniqueone",
+	storageBucket: "dcity-uniqueone.appspot.com",
+	messagingSenderId: "199308763432",
+	appId: "1:199308763432:web:da84ae626f978b638cb01d"
+};
+// Initialize Firebase,
+firebase.initializeApp(firebaseConfig);
